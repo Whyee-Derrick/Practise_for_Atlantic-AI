e32f8851a2117647e41078743e8e27512801a75c
0325080455-netizen <0325080455@htu.edu.gh>
2026-09-18 16:00:48 +0000

    Built an agent using json data

--- Day1/main.py
+++ Day1/main.py
@@ -1,1 +1,31 @@
+import json
+import os
+# try:
+#     with open('courses.json', 'r') as f:
+#         courses = json.load(f)
+# except FileNotFoundError:
+#     print ("The file is not found")        
 
+# def search(user_input):
+#     for course in courses:
+#         if course["code"] == user_input:
+#             return f"{course["code"]} is {course["name"]}\nlevel: {course["level"]}\nSemester:{course["semester"]}."
+#     else:
+#         return "Course not Available!"    
+   
+
+# user_input = input ("What course are you looking for ? ")
+# print(search(user_input))
+
+# while True:    
+#     next_respond = input ("Do you want to look for another course (yes/no) ? ")
+#     if next_respond.lower()== "yes":
+#         user_input = input ("What course are you looking for ? ")
+#         print(search(user_input))
+#     elif next_respond.lower() == "no":
+#         print ("Thanks for your time!😁")
+#         break    
+#     else:
+#         print ("Enter the right respond ")
+print(os.getcwd())
+print(os.listdir())
